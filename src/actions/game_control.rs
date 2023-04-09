85c73bb118ba68bbd2b6df00e8c9343d6f4e3772
Michal Vanko <mklakakilli@gmail.com>
2023-04-09 23:22:09 +0200

    Fix

--- src/actions/game_control.rs
+++ src/actions/game_control.rs
@@ -10,12 +10,12 @@ pub enum GameControl {
 impl GameControl {
     pub fn pressed(&self, keyboard_input: &Res<Input<KeyCode>>) -> bool {
         match self {
-            GameControl::Up => {
-                keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up)
-            }
-            GameControl::Down => {
-                keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down)
-            }
+            // GameControl::Up => {
+            //     keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up)
+            // }
+            // GameControl::Down => {
+            //     keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down)
+            // }
             GameControl::Left => {
                 keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left)
             }
